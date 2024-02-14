1cfe830415058639f2829fda617c14560192bf06
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2024-02-14 09:07:56 +0200

    0

--- src/pages/router.jsx
+++ src/pages/router.jsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "products",
+        index:true,
         element: <Products />
       }
     ],
